d9ae87dd0f2b9a11a0da7907c42c2e9946ec5b7b
Matt <git@mattevans.cloud>
2025-12-16 20:57:04 -0500

    Remove example environment variables file and update .gitignore to streamline ignored files for build output, dependencies, and environment variables.

---
+++ src/config/en/navData.json.ts
@@ -0,0 +1,27 @@
+/**
+ * * This file is used to define the navigation links for the site.
+ */
+
+// types
+import { type navItem } from "../types/configDataTypes";
+
+const navConfig: navItem[] = [
+  {
+    text: "Services",
+    link: "/services/",
+  },
+  {
+    text: "Technologies",
+    link: "/technologies/",
+  },
+  {
+    text: "About",
+    link: "/about/",
+  },
+  {
+    text: "Contact",
+    link: "/contact/",
+  },
+];
+
+export default navConfig;
